655d9be98c823b3bce9b50c25b0d5f4401e55bc9
angelynaliem <angelynaliem@gmail.com>
2020-10-14 22:57:30 -0400

    PrivateRoute built

---
+++ airbnb/src/components/PrivateRoute.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Redirect, Route } from "react-router-dom"
+
+const PrivateRoute = ( { component: Component, ...rest }) => {
+
+    return (
+
+        <Route
+        {...rest}
+        render = {(props) => {
+
+            if(localStorage.getItem("token")) {
+                return <Component {...props} />
+            } else {
+                return <Redirect to="/login" />
+            }
+
+        }}>
+        </Route>
+
+    )
+
+}
+
+export default PrivateRoute
