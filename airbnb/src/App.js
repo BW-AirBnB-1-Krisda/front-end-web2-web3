6ab33817b8e976b015ac26e70015d9964145b649
angelynaliem <angelynaliem@gmail.com>
2020-10-14 22:44:20 -0400

    Added router, switch to App.js

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
 
 import UserLogin from "./components/UserLogin";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <UserLogin />
+      <Switch>
+      <Route exact path="/login" component={UserLogin} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
