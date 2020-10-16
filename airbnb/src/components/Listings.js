883b6a4bbbe2ea3afb2c9fce65f9a058bcf2fb59
angelynaliem <angelynaliem@gmail.com>
2020-10-15 21:34:14 -0400

    Added backend API

--- airbnb/src/components/Listings.js
+++ airbnb/src/components/Listings.js
@@ -1,10 +1,18 @@
 import React from "react"
 
-const Listings = () => {
+const Listings = (props) => {
 
     return (
         <div>
+
+{/* <a href="#" onClick={this.handleClick}></a> */}
+            <a
+            href="/login"
+            onClick={() => window.localStorage.clear()} 
+            >
+            Log out</a>
             <h1>Listings go here with the functionality to get all the listings, add a new listing, edit an existing listing and get the optimal price for the listing, and delete the listing</h1>
+            
         </div>
 
     )
