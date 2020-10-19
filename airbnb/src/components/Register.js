32214fae08bd5679f77df20a78a1cd9eadd6594a
Mark Hillin <mark.hillin@gmail.com>
2020-10-18 21:00:41 -0500

    Yup Installed

--- airbnb/src/components/Register.js
+++ airbnb/src/components/Register.js
@@ -1,82 +1,142 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { register } from "../actions";
 import { connect } from "react-redux";
+import * as Yup from "yup";
+
 
 import { Link } from "react-router-dom";
 import "../App.css";
 
-const initialRegisterForm = {
+function Register () {
 
-    username: "",
-    password: "",
-    email: ""
-}
+    const [userRegister, setUserRegister] = useState({
+        username: "",
+        password: "",
+        email: "",
+        terms: ""
+    })
 
-function Register (props) {
+    const [errors, setErrors] = useState({
+        username: "",
+        password: "",
+        email: "",
+        terms: ""
+      });
 
-    const [userRegister, setUserRegister] = useState(initialRegisterForm)
+    const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
+
+    const validateChange = (e) => {
+        Yup
+          .reach(formSchema, e.target.username)
+          .validate(e.target.value)
+          .then((valid) => {
+            setErrors({ ...errors, [e.target.username]: "" });
+          })
+          .catch((err) => {
+            setErrors({ ...errors, [e.target.username]: err.errors[0] });
+          });
+      };
 
     const handleInputChange = (e) => {
         e.persist()
-        setUserRegister({
+        const newUserRegister = {
             ...userRegister,
-            [e.target.name] : e.target.value
-        })
-    }
+            [e.target.name] : e.target.type === "checkbox" ? e.target.checked : e.target.value
+        };
+        validateChange(e);
+        setUserRegister(newUserRegister);
+    };
+
+    const formSchema = Yup.object().shape({
+        username: Yup
+        .string()
+        .required("Must include username"),
+        password: Yup
+        .string()
+        .required("Password is Required"),
+        email: Yup
+        .string()
+        .email("Must be a valid email address.")
+        .required("Email is Required"),
+        terms: Yup
+        .boolean()
+        .oneOf([true])
+    });
 
     const submitRegisterForm = (e) => {
         e.preventDefault()
-        props.register(userRegister, props.history)
-        
+        setUserRegister({
+            username: "",
+            password: "",
+            email: "",
+            terms: ""
+        })
+        console.log(setUserRegister)
     }
 
+    useEffect(() => {
+        formSchema.isValid(userRegister).then((valid) => {
+          console.log("is my form valid", valid);
+          setButtonIsDisabled(!valid);
+        });
+      }, [formSchema, userRegister]);
+
     return (
-        <div className="App">
+        <div className="register">
             <form onSubmit={submitRegisterForm}>
-            <div className="username">
-                    <label>
+                    <label htmlFor="username">
                         Username:
-                        <input
+                        <input 
                             type="text"
+                            id="username"
                             name="username"
-                            placeholder="Username"
+                            placeholder="username"
+                            value={userRegister.username}
                             onChange={handleInputChange}
                         />
+                        {errors.username.length > 0 ? <p className="error">{errors.username}</p> : null}
                     </label>
-                </div>
-                <div className="password">
-                    <label>
+                    <label htmlFor="password">
                         Password:
-                        <input
+                        <input 
                             type="text"
+                            id="password"
                             name="password"
-                            placeholder="Password"
+                            placeholder="password"
+                            value={userRegister.password}
                             onChange={handleInputChange}
                         />
+                        {errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
                     </label>
-                </div>
-                <div className="email">
-                    <label>
+                    <label htmlFor="email">
                         Email:
                         <input
                             type="text"
                             name="email"
-                            placeholder="Email"
+                            placeholder="email"
+                            value={userRegister.email}
                             onChange={handleInputChange}
                         />
+                        {errors.email.length > 0 ? <p className="error">{errors.email}</p> : null}
                     </label>
-                </div>
-                    <button className="submit">
+                        <label htmlFor="terms" className="terms">
+                            Do you agree to the terms and conditions?
+                            <input 
+                            type="checkbox"
+                            name="terms"
+                            checked={userRegister.terms}
+                            onChange={handleInputChange}
+                            />
+                        </label>
+                    <button className="submit" type="submit" disabled={buttonIsDisabled} >
                         Register
                     </button>
-
                     <div className="loginButton">
-                <Link to="/login">
+                    <Link to="/login">
                     <h5>Already a Member?</h5>
                   <button>Login</button>
                 </Link>
               </div>
-            
             </form>
         </div>
     )
--- airbnb/src/components/UserLogin.js
+++ airbnb/src/components/UserLogin.js
@@ -1,55 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { login } from "../actions";
 import { connect } from "react-redux";
+import * as Yup from "yup";
 
 import { Link } from "react-router-dom";
 import "../App.css";
 
+function UserLogin() {
 
-const initialLoginForm = {
+    const [userLogin, setUserLogin] = useState({
+        username: "",
+        password: ""
+    });
 
-    username: "",
-    password: ""
-}
+    const [errors, setErrors] = useState({
+        username: "",
+        password: ""
+      });
 
-function UserLogin(props) {
+    const [buttonIsDisabled, setButtonIsDisabled] = useState(true);
 
-    const [userLogin, setUserLogin] = useState(initialLoginForm)
+    const validateChange = (e) => {
+        Yup
+          .reach(formSchema, e.target.username)
+          .validate(e.target.value)
+          .then((valid) => {
+            setErrors({ ...errors, [e.target.username]: "" });
+          })
+          .catch((err) => {
+            setErrors({ ...errors, [e.target.username]: err.errors[0] });
+          });
+      };
 
     const handleInputChange = (e) => {
-        e.persist()
-        setUserLogin({
+        e.persist();
+        const newUserLogin = {
             ...userLogin,
             [e.target.name] : e.target.value
-        })
+        };
+        validateChange(e);
+        setUserLogin(newUserLogin);
     }
 
     const submitLoginForm = (e) => {
         e.preventDefault()
-        props.login(userLogin, props.history)
-        
+        setUserLogin({
+            username: "",
+            password: ""
+        })
+        console.log(userLogin)
     }
+
+    const formSchema = Yup.object().shape({
+        username: Yup
+        .string()
+        .required("Must include username"),
+        password: Yup
+        .string()
+        .required("Password is Required")
+    })
+
+    useEffect(() => {
+        formSchema.isValid(userLogin).then((valid) => {
+            console.log(valid)
+            setButtonIsDisabled(!valid)
+        })
+    }, [formSchema, userLogin])
 
     return (
         <div className="login">
         <form onSubmit={submitLoginForm}>
-
-        <div className="username"> 
-            <label>Username:</label>
-                <input
-                minLength='8'
+            <label htmlFor="username">
+                Username:
+                <input 
                 placeholder='username'
                 id='username'
                 name="username"
                 type='text'
                 value={userLogin.username}
                 onChange={handleInputChange}
-                />
-                </div>
-
-            <div className="password">
-            <label>Password:</label>
-                <input
+            />
+                {errors.username.length > 0 ? <p className="error">{errors.username}</p> : null}
+            </label>
+            <label htmlFor="password">
+                Password:
+                <input htmlFor="password"
                 placeholder='password'
                 id='password'
                 name='password'
@@ -57,12 +92,11 @@ function UserLogin(props) {
                 value={userLogin.password}
                 onChange={handleInputChange}
                 />
-                </div>
-
+                {errors.password.length > 0 ? <p className="error">{errors.password}</p> : null}
+            </label>
             <div>
-            <button className="submit">LOGIN</button>
+            <button className="submit" type="submit" disabled={buttonIsDisabled} >LOGIN</button>
             </div>
-           
             <div className="nav-links">
                <div className="registerButton">
                  <Link to="/register">
@@ -71,9 +105,8 @@ function UserLogin(props) {
                  </Link>
                </div>
            </div>
-  
         </form>
-        </div>
+    </div>
     )
 }
 
