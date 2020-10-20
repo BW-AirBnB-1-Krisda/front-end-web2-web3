7e71bcd404d3441574a9527b1bd9d789a73a0ccc
Mark Hillin <mark.hillin@gmail.com>
2020-10-19 20:37:10 -0500

    Monday 2036

--- airbnb/src/components/Listings.js
+++ airbnb/src/components/Listings.js
@@ -3,9 +3,38 @@ import axios from "axios";
 import Listing from "./Listing"
 
 const Listings = (props) => {
+    const [listingData, setListingData] = useState([]);
+    const [city, setCity] = useState(["Chicago"]);
+    console.log(city)
 
+    const effectFn = () => {
+        axios
+        .get(``)
+        .then((res) => setListingData(res));
+    };
+    useEffect(effectFn, [city]);
+
+    const handleChange = (event) => {
+        setCity(event.target.value);
+      };
     return (
         <div>
+            <form>
+                <div className="search">
+                <label htmlFor="city">Search:</label>
+                <input
+                    id="name"
+                    type="text"
+                    name="textfield"
+                    placeholder="Search"
+                    onChange={handleChange}
+                    value={setCity}
+                    />
+                </div>
+            </form>
+            {listingData.map((id) => {
+                return <Listing url={id} city={city} />
+            })};
 
 {/* <a href="#" onClick={this.handleClick}></a> */}
             <a
@@ -13,7 +42,6 @@ const Listings = (props) => {
             onClick={() => window.localStorage.clear()} 
             >
             Log out</a>
-            <Listing />
         </div>
 
     )
