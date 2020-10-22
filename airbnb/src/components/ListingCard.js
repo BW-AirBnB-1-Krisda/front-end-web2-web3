0fccb43f590a7430fedb9955ecef64f7f613d488
angelynaliem <angelynaliem@gmail.com>
2020-10-22 12:12:32 -0400

    Replaced Listing with ListingCard, incorporating changes

--- airbnb/src/components/ListingCard.js
+++ airbnb/src/components/ListingCard.js
@@ -5,6 +5,9 @@ import { connect } from "react-redux";
 
 import { useHistory } from "react-router-dom";
 
+import ControlPointIcon from '@material-ui/icons/ControlPoint';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 const ListingCard = (props) => {
 
     const history = useHistory();
@@ -17,20 +20,43 @@ const ListingCard = (props) => {
     }
 
     return (
-        <div>
-            <h2>Listing card</h2>
+        <div className="listing">
+        <h1>{props.city}</h1>
+            <img src="https://a0.muscache.com/im/pictures/b0c6aa89-c43e-424b-89f0-34b27b840a9c.jpg?im_w=1200" alt="airbnb"></img>
+        <div className="attributes">
 
-            <p>City: {props.listing.city}</p>
-            <p>Room type: {props.listing.room_type}</p>
-            <p>Security deposit: {props.listing.security_deposit}</p>
-            <p>Guests included: {props.listing.guests_included}</p>
-            <p>Min. no. of nights: {props.listing.min_nights}</p>
-            {/* <p>OPTIMAL Price: {props.listing.price}</p> */}
-
-            <button onClick={() => history.push(`/listings/${props.listing.id}`)}>Edit Listing</button>
-            <button onClick={deleteListingCard}>Delete Listing</button>
-
+        <ul>
+                <li>City: {props.listing.city}</li>
+                <li>Room Type: {props.listing.room_type}</li>
+                <li>Security Deposit: ${props.listing.security_deposit}</li>
+                <li>Max Guests: {props.listing.guests_included}</li>
+                <li>Min Number of Nights: {props.listing.min_nights}</li>
+                {/* <li>OPTI Price: ${props.listing.price}</li> */}
+            </ul>
         </div>
+
+        <div className="buttons">
+
+        <button className="topchill-button" onClick={() => history.push(`/listings/${props.listing.id}`)}>
+            <div className="control">
+                <ControlPointIcon color="white" />
+            </div>
+            <div>
+                Edit Listing
+            </div>
+        </button>
+
+        <button className="topchill-button" onClick={deleteListingCard}>>
+            <div className="control">
+                <DeleteIcon color="white" />
+            </div>
+            <div>
+                Delete Listing
+            </div>
+        </button>
+        </div>
+        </div>
+       
     )
 }
 
